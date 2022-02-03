5f8326ad1c65222ebd25bafceff2c7882acaad48
Atsushi Togo <atz.togo@gmail.com>
2022-02-03 17:08:18 +0900

    Fix test of keyset4

--- tests/calculations/test_phono3py.py
+++ tests/calculations/test_phono3py.py
@@ -22,41 +22,6 @@ def generate_inputs(generate_structure, generate_nac_params):
     return _generate_inputs
 
 
-def test_phono3py_with_nac(
-    fixture_sandbox,
-    fixture_code,
-    generate_calc_job,
-    generate_inputs,
-    generate_settings,
-):
-    """Test a phonopy calculation."""
-    entry_point_calc_job = "phonoxpy.phono3py"
-
-    inputs = generate_inputs(
-        metadata={"options": {"resources": {"tot_num_mpiprocs": 1}}}, with_nac=True
-    )
-    inputs.update(
-        {
-            "settings": generate_settings(mesh=50, isotope=True),
-            "code": fixture_code(entry_point_calc_job),
-        }
-    )
-
-    ph_settings = {}
-    _setup_phono3py_calculation_keyset4(
-        ph_settings, inputs["settings"], run_ltc=True, run_fc=True
-    )
-    assert set(ph_settings) == set(("mesh", "isotope"))
-
-    calc_info = generate_calc_job(fixture_sandbox, entry_point_calc_job, inputs)
-    assert set(calc_info.retrieve_list) == set(
-        ("phono3py.yaml", "fc2.hdf5", "fc3.hdf5")
-    )
-    assert set(calc_info.codes_info[0].cmdline_params) == set(
-        ("-c", "phono3py_params.yaml.xz", "--sym-fc", "--compact-fc", "--nac")
-    )
-
-
 def test_phono3py_fc(
     fixture_sandbox,
     fixture_code,
@@ -87,6 +52,55 @@ def test_phono3py_fc(
     )
     assert set(calc_info.codes_info[0].cmdline_params) == set(
         ("-c", "phono3py_params.yaml.xz", "--sym-fc", "--compact-fc")
+    )
+
+
+def test_phono3py_with_ltc_nac(
+    fixture_sandbox,
+    fixture_code,
+    generate_calc_job,
+    generate_inputs,
+    generate_settings,
+    generate_fc3_filedata,
+    generate_fc2_filedata,
+):
+    """Test a phonopy calculation."""
+    entry_point_calc_job = "phonoxpy.phono3py"
+
+    inputs = generate_inputs(
+        metadata={"options": {"resources": {"tot_num_mpiprocs": 1}}}, with_nac=True
+    )
+    inputs.update(
+        {
+            "settings": generate_settings(
+                mesh=100,
+                phonon_supercell_matrix=[2, 2, 2],
+            ),
+            "code": fixture_code(entry_point_calc_job),
+            "fc2": generate_fc2_filedata(),
+            "fc3": generate_fc3_filedata(),
+        }
+    )
+
+    ph_settings = {}
+    _setup_phono3py_calculation_keyset4(ph_settings, inputs["settings"], run_ltc=True)
+    assert set(ph_settings) == set(("mesh",))
+
+    calc_info = generate_calc_job(fixture_sandbox, entry_point_calc_job, inputs)
+    assert set(calc_info.retrieve_list) == set(("phono3py.yaml", "kappa-*.hdf5"))
+    assert set(calc_info.codes_info[0].cmdline_params) == set(
+        (
+            "-c",
+            "phono3py_params.yaml.xz",
+            "--nac",
+            "--fc2",
+            "--fc3",
+            "--mesh",
+            "100.0",
+            "--br",
+            "--ts",
+            "300",
+        )
     )
 
 
